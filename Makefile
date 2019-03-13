b798b855b66e4b9ffbdec3c6c49e4dbbeac596f0
Adolfo Ashara dayne <aashara-@e3r8p5.21-school.ru>
2019-03-13 16:41:04 +0300

    ft_ls 13.03.19 fix norms

--- Makefile
+++ Makefile
@@ -6,25 +6,25 @@
 #    By: aashara- <aashara-@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/01/22 12:59:55 by aashara-          #+#    #+#              #
-#    Updated: 2019/03/13 14:13:02 by aashara-         ###   ########.fr        #
+#    Updated: 2019/03/13 16:40:30 by aashara-         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME=ft_ls
 
 SRC=srcs/reading.c\
-	srcs/make.c\
 	srcs/flags.c\
 	srcs/check2.c\
 	srcs/check.c\
+	srcs/make.c\
 	srcs/check3.c\
-	srcs/sort_list.c\
 	srcs/output.c\
+	srcs/sort_list.c\
+	srcs/sort_list2.c\
 	srcs/print_rows.c\
 	srcs/print_rows2.c\
 	srcs/print_rows3.c\
 	srcs/print_cols.c\
-	srcs/sort_list2.c\
 	srcs/print_cols2.c\
 
 INCLUDES=includes
--- includes/ft_ls.h
+++ includes/ft_ls.h
@@ -6,7 +6,7 @@
 /*   By: aashara- <aashara-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/22 13:00:16 by aashara-          #+#    #+#             */
-/*   Updated: 2019/03/13 15:48:34 by aashara-         ###   ########.fr       */
+/*   Updated: 2019/03/13 16:39:48 by aashara-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,7 +75,6 @@ typedef struct	s_prt_rows
 }				t_prt_rows;
 
 t_dir			*opening(int argc, char **argv);
-t_dir			*ft_list(ushort flags);
 ushort			read_flags(char **argv, uint8_t *i);
 ushort			add_flag(ushort flags, char flag);
 ushort			add_flag2(ushort flags, char flag);
@@ -83,37 +82,39 @@ void			print_usage(char c);
 char			**check_dir(int argc, char **argv, uint8_t i);
 uint8_t			double_arr_len(char **d_names);
 t_dir			*make_list(char **arr, uint8_t *i, ushort flags);
-char			get_type(mode_t mode);
-DIR		*check_open(char *path, char *name);
-void			check_close(int nb);
 t_dir			*check_exist(t_dir *dir, t_dir **head, ushort flags);
-t_dir			*sort_one_list(t_dir *list, uint8_t (fun)(t_dir*));
-uint8_t			list_sort(t_dir *list);
-t_dir			*swap_list(t_dir *cur, t_dir *next);
+t_dir			*ft_list(ushort flags);
+char			*check_name(char *name);
+t_dir			*get_data(t_dir **request);
+void			check_err(char *name, char *path);
+void			delete_from_list(t_dir **dir, t_dir **head);
+void			free_list(t_dir **request);
+char			get_type(mode_t mode);
+t_dir			*make_file_list(t_dir *dir, t_dir **head_files, t_dir **head);
+t_dir			*make_dir_list(t_dir **head, t_dir *dir);
+t_dir			*print_files(t_dir *head, t_dir *head_files);
+void			print(t_dir *request);
+t_dir			*sorting(t_dir *request, ushort flags);
 ushort			is_flags(ushort flags, char flag);
 ushort			is_flags2(ushort flags, char flag);
-t_dir			*reading(t_dir *list);
-char			*check_path(char *path, char *name);
-
-t_dir	*get_data(t_dir **request);
-void	delete_from_list(t_dir **dir, t_dir **head);
-t_dir	*make_file_list(t_dir *dir, t_dir **head_files, t_dir **head);
-void	free_all_list(t_dir *request);
-t_dir	*make_dir_list(t_dir **head, t_dir *dir);
-char	*check_name(char *name);
-void		print_line(t_dir *request, t_prt_cols pprm, char *buf, u_int16_t *i);
-void		print_rows(t_dir *request, ushort ws_cols, ushort flags, uint8_t i);
-void	print_all_rek(t_dir *request, ushort size,
-		void (f)(t_dir *, ushort, ushort, uint8_t), ushort flags);
-void		print_cols(t_dir *request, ushort ws_col, ushort flags, uint8_t j);
-t_dir	*print_files(t_dir *head, t_dir *head_files);
-void	check_err(char *name, char *path);
-void	data_init(struct dirent *file, t_dir *list, t_dir **d, t_dir **head);
-u_int8_t check_buf(char *buf, u_int8_t i);
-void	get_cols_rows(t_prt_cols *pprm, ushort ws_col);
-void	print_path(t_dir *request, ushort flags);
-
-void			print(t_dir *request);
+t_dir			*sort_one_list(t_dir *list, uint8_t (fun)(t_dir*));
+t_dir			*swap_list(t_dir *cur, t_dir *next);
+uint8_t			list_sort(t_dir *list);
+uint8_t			list_f_d(t_dir *list);
+uint8_t			list_rev(t_dir *list);
+uint8_t			list_time_mod(t_dir *list);
+uint8_t			list_time_a(t_dir *list);
+uint8_t			list_size(t_dir *list);
+void			print_cols(t_dir *request, ushort ws_col, ushort flags,
+				uint8_t j);
+t_prt_cols		get_print_prm_c(t_dir *request, ushort ws_col);
+void			get_cols_rows(t_prt_cols *pprm, ushort ws_col);
+void			print_line(t_dir *request, t_prt_cols pprm, char *buf,
+				u_int16_t *i);
+u_int8_t		check_buf(char *buf, u_int8_t i);
+t_dir			*next_elem(t_dir *request, t_prt_cols pprm);
+void			print_rows(t_dir *request, ushort ws_cols, ushort flags,
+				uint8_t i);
 t_prt_rows		get_print_prm_r(t_dir *request);
 uint8_t			get_bit(int nlink);
 void			print_line_rows(t_dir *request, ushort flags, t_prt_rows pprm);
@@ -128,13 +129,15 @@ void			print_elem(char *str, uint8_t max);
 void			print_time(time_t time);
 void			print_link(t_dir *request);
 void			print_attr_full(t_dir *request, ushort flags);
-t_prt_cols		get_print_prm_c(t_dir *request, ushort ws_col);
-t_dir			*next_elem(t_dir *request, t_prt_cols pprm);
-uint8_t			list_f_d(t_dir *list);
-t_dir			*sorting(t_dir *request, ushort flags);
-uint8_t			list_rev(t_dir *list);
-uint8_t			list_time_mod(t_dir *list);
-uint8_t			list_time_a(t_dir *list);
-uint8_t			list_size(t_dir *list);
-void			free_list(t_dir **request);
+void			print_all_rek(t_dir *request, ushort size,
+				void (f)(t_dir *, ushort, ushort, uint8_t), ushort flags);
+t_dir			*reading(t_dir *list);
+DIR				*check_open(char *path, char *name);
+void			data_init(struct dirent *file, t_dir *list, t_dir **d,
+				t_dir **head);
+char			*check_path(char *path, char *name);
+void			check_close(int nb);
+void			print_path(t_dir *request, ushort flags);
+void			free_all_list(t_dir *request);
+
 #endif
